8a9f0ce262b8ce4de85274ed2c5f993319cd114d
Joudomar46 <77951776+Joudomar46@users.noreply.github.com>
2022-12-07 19:11:33 -0500

    Updated Home Page
    New images and css for home and navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,34 @@
-
-
 export default function Home() {
     return(
-        <>Hi</>
-        // <div className="main">
-        //     <div class="flexrowOne">
-        //         <img src={require('./solar1.jpg')} height="30%" width="30%"/>
-                
-        //         <div class="headText">
-        //             <h1>Welcome to EZWatts!</h1>
-        //                 <a class="createbutton" href="Create">Click to Begin</a>
-        //         </div>
-        //         <img src={require('./solar2.jpg')} height="30%" width="30%"/>
+        //main 
+        <div className="main-home"> 
+        {/* right images row */}
+            <div class="flexrowOne">
+                <img class="home-images" src={require('../media/solar1.jpg')} alt="" height="30%" width="30%"/>
+                <div class="headText">
+                    <h1>Welcome to EZWatts!</h1>
+                        <a class="createbutton" href="./create/Create">Click to Begin</a>
+                </div>
+                <img class="home-images" src={require('../media/solar2.jpg')} alt="" height="30%" width="30%"/>
 
-        //     </div>
-
-        //     <div class="flexrowTwo">
-        //         <img src={require('./solar3.jpg')} height="30%" width="30%"/>
-        //         <div class="middleText">
-        //         <p class="main_description">
-        //             Here at EZWatts we believe that a person should be
-        //             able to have ease of access when implementing
-        //             solar panels for one's house.  Whether it is a company
-        //             or an individual, with our program you can set an outline
-        //             of your roof, point out your obstructions, and get an accurate
-        //             representation and price for the placement and installation 
-        //             of solar panels.
-        //         </p>
-        //         </div>
-
-        //         <img src={require('./solar4.jpg')} height="30%" width="30%"/>
-        //     </div>
-        //     </div>
+            </div>
+        {/* left images row */}
+            <div class="flexrowTwo">
+                <img class="home-images" src={require('../media/solar3.jpg')} alt="" height="30%" width="30%"/>
+                <div class="middleText">
+                <p class="main-description">
+                    Here at EZWatts we believe that a person should be
+                    able to have ease of access when implementing
+                    solar panels for one's house.  Whether it is a company
+                    or an individual, with our program you can set an outline
+                    of your roof, point out your obstructions, and get an accurate
+                    representation and price for the placement and installation 
+                    of solar panels.
+                </p>
+                </div>
+                <img class="home-images" src={require('../media/solar4.jpg')} alt="" height="30%" width="30%"/>
+            </div>
+            </div>
         
         
 
