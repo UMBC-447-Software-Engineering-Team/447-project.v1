4b445e5800181d6af1731c891d2fa3d5ca21dffe
Joudomar46 <77951776+Joudomar46@users.noreply.github.com>
2022-12-07 19:14:13 -0500

    Updated ContactUs.js

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,5 +1,15 @@
-export default function Home() {
-    return(
-        <>Hi</>
+export default function Contact() {
+    return (
+        <div class="contacts">
+        <p>Welcome to the Contact Us page.  Below are our ways of contact.</p>
+        <p><b>Phone:  410-399-2887</b></p>
+        <p><b>Email:  ezwatts@gmail.com</b></p>
+        <p><b>Other Contacts</b></p>
+        <p>Ace</p>
+        <p>Joud</p>
+        <p>Marc</p>
+        <p>Tanner</p>
+        <p>Zoe</p>
+    </div>
     )
-}
+  } 
