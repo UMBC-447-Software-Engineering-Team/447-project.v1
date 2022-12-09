f1aed10e1215d0c3675b22f7fa0d96887df4c048
Joudomar46 <77951776+Joudomar46@users.noreply.github.com>
2022-12-09 12:30:00 -0500

    Cleaned up comments

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -7,17 +7,15 @@ import React, { useLayoutEffect, useState, useRef } from 'react';
 
 
 export function Create() {
+  // Canvas Dimentions
   let canvasWidth = 1000;
   let canvasHeight = 1000;
-  // for testing 20,10,600,600,30,10,20
+  // Variables in order to access the submitted inputs outside of handleSubmit hook
   const [panelWidths, setPanelWidths] = useState(0);
   const [panelHeights, setPanelHeights] = useState(0);
   const [roofWidths, setRoofWidths] = useState(0);
   const [roofHeights, setRoofHeights] = useState(0);
-  const [rowSpacings, setRowSpacing] = useState(0);
-  const [columnSpacing, setColumnSpacing] = useState(0);
   const [edgeSpacings, setEdgeSpacings] = useState(0);
-
 
   const [juristictionWidth, setJuristictionWidth] = useState(0);
   const [juristictionHeight, setJuristictionHeight]= useState(0);
@@ -25,157 +23,107 @@ export function Create() {
   const [numPanelsWide, setNumPanelsWide] = useState(0);
   const [numPanelsTall, setNumPanelsTall]= useState(0);
 
+  // Initialize a new array
   const [array, setArray] = useState(new Array());
   
+    // Where the magic of hitting the submit button on the Form happens
+    function handleSubmit(panelWidth, panelHeight, roofWidth, 
+      roofHeight, rowSpacing, columnSpacing, edgeSpacing) {
 
-  function handleSubmit(panelWidth, panelHeight, roofWidth, 
-    roofHeight, rowSpacing, columnSpacing, edgeSpacing) {
-    
-    
-    const roofWidths = +roofWidth;
-    setRoofWidths(roofWidths);
+      // copy over the newly changed inputs using + to convert from string to number
+      const roofWidths = +roofWidth;
+      setRoofWidths(roofWidths);
 
-    const roofHeights = +roofHeight;
-    setRoofHeights(roofHeights);
+      const roofHeights = +roofHeight;
+      setRoofHeights(roofHeights);
 
-    const panelWidths = +panelWidth;
-    setPanelWidths(panelWidths)
+      const panelWidths = +panelWidth;
+      setPanelWidths(panelWidths)
 
-    const panelHeights = +panelHeight;
-    setPanelHeights(panelHeights);
+      const panelHeights = +panelHeight;
+      setPanelHeights(panelHeights);
 
-    const edgeSpacings = +edgeSpacing;
-    setEdgeSpacings(edgeSpacings);
+      const edgeSpacings = +edgeSpacing;
+      setEdgeSpacings(edgeSpacings);
 
-
-    //Get juristiction
-    const juristictionWidth =  Math.floor(+panelWidth + +columnSpacing); //expected = 30
-    const juristictionHeight =  Math.floor(+panelHeight + +rowSpacing); //expected = 40
-
-    setJuristictionWidth(juristictionWidth); 
-    setJuristictionHeight(juristictionHeight);
-    // console.log(`The sum of ${panelWidth} and ${columnSpacing} is ${juristictionWidth}.`);
-
-
-    // Calculate the number of panels that will fit on the roof
-    // 600 - 40 = 560 + 10 = 570 / 30
-    const numPanelsWide = Math.floor(((+roofWidth - (+edgeSpacing * 2)) + +columnSpacing) /juristictionWidth);
-    const numPanelsTall = Math.floor(((+roofHeight - (2 * +edgeSpacing)) + +rowSpacing) / juristictionHeight);
-    setNumPanelsWide(numPanelsWide)
-    setNumPanelsTall(numPanelsTall)
-
-    //  // Create a 2D array with the specified length and width
-    //  let array = Array.from({length: numPanelsWide}, () => Array.from({length: numPanelsTall}, () => []));
-    
-     // Create an empty 2D array
-    
-
-    
-    
-    let newArray = new Array(numPanelsTall);
-    // Row by column aka PanelsTall by Panels Wide
-    for (let i = 0; i < numPanelsTall; i++) {
-      console.log('here')
-      newArray[i] = new Array(numPanelsWide);
-    }
-    console.log(`The NEW array initialization  .`);
-    console.log(newArray)
-  
-    //  let array = Array({numPanelsWide}, () => Array.from({numPanelsTall}, () => []));
-
-    // console.log(`The new array after filling.`);
-    // console.log(newArray)
-    // to use outside hook
-    setArray(null);
-    setArray(newArray);
-    // Deallocate the old array
-    newArray = null;
-     // Log the array to the console
-    // console.log(`The new array NULL.`);
-    // console.log(newArray)
-
-    console.log(`The array after filling .`);
-    console.log(array)
-
-
-}
-
-
-  //empty array thats filled with total panels possible ie pColumnCount * pRowCount
-
-
-
-  const canvasRef = useRef(null);
-  // Declare an empty 2D array with rows and columns
-
-
-    // Row by column aka PanelsTall by Panels Wide
-    // add items to array
-    let xCoord = edgeSpacings;
-    let yCoord = edgeSpacings;
-
-     for (let i = 0; i < numPanelsTall; i++) {  //for each row 
-      for (let j = 0; j < numPanelsWide; j++) {   //each column
-        array[i][j] = [xCoord , yCoord];
-        xCoord = xCoord + juristictionWidth;
-      }
-      xCoord = edgeSpacings;
-      yCoord = yCoord + juristictionHeight;
-    }
-
-  console.log(`The array after filling outside hook  .`);
-  console.log(array)
-
-    // for (let i = 0; i < numPanelsWide; i++){
-    //     panelsArr[i] = {};
-    //     for (let j = 0; j < numPanelsTall; j++){
-    //         panelsArr[i][j] = {x:xCoord, y:yCoord, status:1}; //status 1 = true, 0 = delete
-    //         yCoord = yCoord + juristictionHeight;
-    //     }
-    //     xCoord = xCoord + juristictionWidth;
-    //     yCoord = edgeSpacings
-    // }
-
-
-  useLayoutEffect( () => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    // clear the canvas on each submit
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    // const canvas = document.getElementById("panelCan");
-    // const ctx = canvas.getContext("2d");
-    
-    //for visulization of buiilding
-    ctx.strokeRect(0,0,roofWidths,roofHeights); 
-
-    //for visulization of usable building space on building 
-    var tempBuildWidth =  Math.floor(roofWidths - (edgeSpacings * 2));
-    var tempBuildHeight =  Math.floor(roofHeights - (edgeSpacings * 2));
-    
-    ctx.fillStyle = "green";  //box inside the outer canvas or grid
-    ctx.fillRect(edgeSpacings, edgeSpacings, tempBuildWidth, tempBuildHeight);
- 
-
-    //starting coordinates for the first top right of the box 
-    // draw the array
-    for (let i = 0; i < numPanelsTall; i++) {
-      for (let j = 0; j < numPanelsWide; j++) {
-        const [tempX,tempY] = array[i][j];
-        //draw panel               
-        ctx.fillStyle = "red";
-        ctx.fillRect(tempX, tempY, panelWidths, panelHeights);
+      //Get juristiction which is the space around a panel width and hight wise
+      const juristictionWidth =  Math.floor(+panelWidth + +columnSpacing); 
+      const juristictionHeight =  Math.floor(+panelHeight + +rowSpacing);
       
+      // copy over the newly changed inputs using + to convert from string to number
+      setJuristictionWidth(juristictionWidth); 
+      setJuristictionHeight(juristictionHeight);
+
+      // Calculate the number of panels that will fit on the roof
+      // This accounts for the edgespace and row/column spacing overlap
+      const numPanelsWide = Math.floor(((+roofWidth - (+edgeSpacing * 2)) + +columnSpacing) /juristictionWidth);
+      const numPanelsTall = Math.floor(((+roofHeight - (2 * +edgeSpacing)) + +rowSpacing) / juristictionHeight);
+      setNumPanelsWide(numPanelsWide)
+      setNumPanelsTall(numPanelsTall)
+
+      // Create a temp array based on new total number of panels in a row and column 
+      // Row = Panels Tall, Column = Panels wide 
+      let newArray = new Array(numPanelsTall);
+      // Row by column aka PanelsTall by Panels Wide
+      for (let i = 0; i < numPanelsTall; i++) {
+        newArray[i] = new Array(numPanelsWide);
       }
+
+      // deallocate array
+      setArray(null);
+      // to use outside hook copy temp array to array 
+      setArray(newArray);
+      // Deallocate the old array
+      newArray = null;
     }
 
-  });
+  // for the canvas
+  const canvasRef = useRef(null);
 
+  // add coordinanted for each array
+  // first coordinant starts at x and y = edgeSpacing from the upper left
+  let xCoord = edgeSpacings;
+  let yCoord = edgeSpacings;
+    for (let i = 0; i < numPanelsTall; i++) {  //for each row 
+    for (let j = 0; j < numPanelsWide; j++) {   //each column
+      array[i][j] = [xCoord , yCoord];
+      xCoord = xCoord + juristictionWidth;
+    }
+    xCoord = edgeSpacings;
+    yCoord = yCoord + juristictionHeight;
+  }
+
+    useLayoutEffect( () => {
+      //refrence current canvas
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext('2d');
+      // clear the canvas on each submit
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
  
+      // Draw a box around the whole roof for visulization
+      ctx.strokeRect(0,0,roofWidths,roofHeights); 
+
+      // Draw green square around USABLE building space 
+      var tempBuildWidth =  Math.floor(roofWidths - (edgeSpacings * 2));
+      var tempBuildHeight =  Math.floor(roofHeights - (edgeSpacings * 2));
+      ctx.fillStyle = "green";  //box inside the outer canvas or grid
+      ctx.fillRect(edgeSpacings, edgeSpacings, tempBuildWidth, tempBuildHeight);
+  
+      // draw the array
+      for (let i = 0; i < numPanelsTall; i++) {
+        for (let j = 0; j < numPanelsWide; j++) {
+          const [tempX,tempY] = array[i][j];
+          //draw panel               
+          ctx.fillStyle = "red";
+          ctx.fillRect(tempX, tempY, panelWidths, panelHeights);
+        
+        }
+      }
+
+    });
 
   return (
     <>
-
-      
       <div className="main">
       <div className="Left">
           <Form onSubmit={handleSubmit} />
@@ -183,21 +131,10 @@ export function Create() {
           and ${juristictionWidth} and ${juristictionHeight} .`);
       </div>
       <div className="Right">
-      {/* <pre>
-        {JSON.stringify(panelsArr, null, 1)}
-      </pre> */}
       <canvas ref={canvasRef} width= {canvasWidth} height={canvasHeight} />
-    
-    
         </div>
-       
       </div>
-  
      </>
-       
-    
-  
   );
 }
 
-// AIzaSyBRMZAbu5zpQqLqB8wNz5ntesPcxs0SZkY
