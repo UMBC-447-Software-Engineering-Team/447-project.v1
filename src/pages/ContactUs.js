86b3483a9db62f615d55072e69cf6f773110b331
acema <ace.marino@gmail.com>
2022-12-10 22:52:06 -0500

    updating fortune cookie
    attempted to add in an animated cookie. currently only have a clicked cookie image that shows a random fortune

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -4,21 +4,42 @@ import { useState } from "react";
 
 export default function Contact() {
 
-  const fortunes = [
+  var Fortunes = [
     "A friend is a treasure",
     "An enemy is a lesson",
     "A journey is a reward",
     "Life is an adventure",
-    "Love is a gift"
+    "Love is a gift",
+    "You will be attacked next Wednesday at 3:15 P.M. by six samurai sword wielding purple fish glued to Harley-Davidson motorcycles.",
+    "Excellent time to become a missing person."
   ];
       
-  const [fortune, setFortune] = useState(null);
-      
+  const [Fortune, setFortune] = useState(null);
+  var fortuneText;
   function generateFortune() {
-    const index = Math.floor(Math.random() * fortunes.length);
-    setFortune(fortunes[index]);
+    const index = Math.floor(Math.random() * Fortunes.length);
+    setFortune(Fortunes[index]);
+    
   }
-      
+  
+  function nextState(){
+    let elClass = this.classList,
+      spawned = "spawned",
+      opened = "opened";
+
+    // open cookie
+    if (elClass.contains(spawned)) {
+      elClass.remove(spawned);
+      elClass.add(opened);
+
+    // new cookie
+    } else {
+      elClass.remove(opened);
+      elClass.add(spawned);
+      generateFortune();
+    }
+  };
+
   return (
     <div class="contacts">
     <p>Welcome to the Contact Us page.  Below are our ways of contact.</p>
@@ -30,8 +51,27 @@ export default function Contact() {
     <p>Marc</p>
     <p>Tanner</p>
     <p>Zoe</p>
-    <button id="cookie" onClick={generateFortune}>Click me for a fortune!</button>
-    {fortune && <p>{fortune}</p>}
+    <button class="fc spawned" type="button" onClick={generateFortune}>
+	<div class="fc-part left"></div>
+	<div class="fc-part right"></div>
+	<div class="fc-crumbs">
+		<div class="fc-crumb"></div>
+		<div class="fc-crumb"></div>
+		<div class="fc-crumb"></div>
+		<div class="fc-crumb"></div>
+		<div class="fc-crumb"></div>
+		<div class="fc-crumb"></div>
+		<div class="fc-crumb"></div>
+		<div class="fc-crumb"></div>
+	</div>
+	<div class="fc-fortune">
+		<p class="fc-fortune-text">no</p>
+		<p class="fc-lucky-numbers">Lucky Numbers <span>?, ?, ?, ?, ?, ?</span></p>
+	</div>
+</button>
+
+
+    {Fortune && <p>{Fortune}</p>}
     </div>
   )
 } 
